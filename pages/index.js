007aaef724cd339fd34b047559134503e3ed64b6
Ankita <ankitakumawat111@gmail.com>
2024-02-24 17:30:26 +0530

    update home page

--- pages/index.js
+++ pages/index.js
@@ -61,13 +61,13 @@ export default function Home() {
 
   return (
     <div>
-      <img src='/google-meet-logo.png' alt='Logo' height={300} width={300} className='pl-10' />
-      <div className='bg-white  flex items-center sm:flex-col xsm:flex-col xsm:ml-8 xsm:mr-8 xl:flex-row xl:ml-20 xl:mr-20'>
+      <img src='/google-meet-logo.png' alt='Logo' height={200} width={200} className=' m-4' />
+      <div className='bg-white  flex items-center xl:pt-12 xl:p-36 sm:flex-col xsm:flex-col xsm:ml-8 xsm:mr-8 xl:flex-row xl:ml-20 xl:mr-20'>
         <div className={styles.videoContainer}>
           <video ref={videoRef} autoPlay muted playsInline width={650} className=' rounded-lg' />
         </div>
         <div className={styles.homeContainer}>
-          <h1>Google Meet Clone</h1>
+          {/* <h1>Google Meet Clone</h1> */}
 
           <div className={styles.enterRoom}>
             <input
